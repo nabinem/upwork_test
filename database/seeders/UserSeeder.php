94d0cdf9478d73eb4e10c041d86d0574f07ee6f5
Nabin Nembang <yakthumbanabinem@gmail.com>
2024-01-25 18:47:59 +0545

    seed users table with wallet

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+use Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@upwork-testlocal',
+            'password' => Hash::make('12345678'),
+            'wallet' => 10
+        ]);
+    }
+}
